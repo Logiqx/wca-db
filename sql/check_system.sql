63b21f67d5737bf32499f8db9001ad8e23a31e48
Logiqx <software@mikeg.me.uk>
2019-05-07 14:04:12 +0100

    Tidy up

--- sql/check_system.sql
+++ sql/check_system.sql
@@ -6,15 +6,6 @@
     Purpose:  Check server configuration and status
 */
 
--- TODO - temp tables
-SELECT @@tmp_table_size / 1024 / 1024; -- 32 MB -> 512MB
-SELECT @@max_heap_table_size / 1024 / 1024; -- 32 MB -> 512MB
-SHOW GLOBAL STATUS LIKE 'created_tmp_%tables'; -- Note that this command in itself creates a temporary table
-
--- TODO - query cache
-show variables like 'have_query_cache';
-show variables like 'query_cache_%' ;
-
 -- InnoDB Buffer Pool
 SELECT @@innodb_buffer_pool_size / 1024 / 1024; -- 128 MB -> 2048 MB
 SHOW VARIABLES LIKE 'innodb_buffer_pool_instances'; -- 8
@@ -23,8 +14,12 @@ SHOW VARIABLES LIKE 'innodb_buffer_pool_instances'; -- 8
 SELECT @@innodb_log_file_size / 1024 / 1024;  -- 48 MB -> 512MB
 SHOW VARIABLES LIKE 'innodb_log_files_in_group'; -- 2
 
+-- InnoDB Temp Tables
+SELECT @@tmp_table_size / 1024 / 1024; -- 32 MB -> 512MB
+SELECT @@max_heap_table_size / 1024 / 1024; -- 32 MB -> 512MB
+
 -- Data Import and Export Operations
-SHOW VARIABLES LIKE 'secure_file_priv'; -- NULL -> empty
+SHOW VARIABLES LIKE 'secure_file_priv'; -- NULL -> /home/jovyan/work/
 
 -- InnoDB Flush Log at Transaction Commit
 SHOW VARIABLES LIKE 'innodb_flush_log_at_trx_commit'; -- 1 -> 2 
@@ -39,7 +34,11 @@ SHOW VARIABLES LIKE 'innodb_doublewrite'; -- ON
 SHOW VARIABLES LIKE 'innodb_use_native_aio'; -- ON
 
 -- InnodDB Log Buffer Size
-SELECT @@innodb_log_buffer_size / 1024 / 1024;  -- 8MB
+SELECT @@innodb_log_buffer_size / 1024 / 1024;  -- 8 MB
+
+-- Query cache
+SHOW VARIABLES LIKE 'have_query_cache'; -- YES
+SHOW VARIABLES LIKE 'query_cache_%' ;
 
 -- Auto Commit
 SHOW VARIABLES LIKE 'autocommit'; -- ON
@@ -60,6 +59,9 @@ SHOW ENGINE INNODB STATUS;
 
 -- InnoDB Log Waits (>0 implies the log buffer is too small)
 SHOW GLOBAL STATUS  LIKE 'innodb_log_waits'; -- 0
+
+-- InnoDB Temp Tables - Note that this command in itself creates a temporary table
+SHOW GLOBAL STATUS LIKE 'created_tmp_%tables';
 
 -- InnoDB OS Log Written
 SHOW GLOBAL STATUS LIKE 'innodb_os_log_written';
