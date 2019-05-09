6c7b99e46f886a163af5727e022505663235c91a
Logiqx <software@mikeg.me.uk>
2019-05-09 17:28:59 +0100

    Enhanced the table and index checks to compare databases

--- sql/check_indices.sql
+++ sql/check_indices.sql
@@ -6,50 +6,46 @@
     Purpose:  Check indices on the WCA tables
 */
 
-SET @wca_db = 'wca';
-
--- List index statistics
-SELECT *
-FROM information_schema.statistics
-WHERE table_schema = @wca_db
-ORDER BY table_name, table_schema;
+SET @wca_db = 'wca_alt';
 
 -- List tables with primary key
 SELECT t.table_schema, t.table_name, s.index_name, t.table_rows,  
 	MAX(s.cardinality) AS index_cardinality, 1 - MIN(s.non_unique) AS index_unique
 FROM information_schema.tables t
 LEFT JOIN information_schema.statistics s ON s.table_schema = t.table_schema AND s.table_name = t.table_name
-WHERE t.table_schema = @wca_db
+WHERE t.engine = 'InnoDB'
+AND t.table_schema = @wca_db
 AND s.index_name ='PRIMARY'
 GROUP BY t.table_schema, t.table_name, t.table_rows,  s.index_name
 ORDER BY t.table_schema, t.table_name, t.table_rows,  s.index_name;
 
--- List tables with one or more indices
+-- List tables with one or more secondary indices
 SELECT t.table_schema, t.table_name, s.index_name, t.table_rows,  
 	MAX(s.cardinality) AS index_cardinality, 1 - MIN(s.non_unique) AS index_unique
 FROM information_schema.tables t
 LEFT JOIN information_schema.statistics s ON s.table_schema = t.table_schema AND s.table_name = t.table_name
-WHERE t.table_schema = @wca_db
+WHERE t.engine = 'InnoDB'
+AND t.table_schema = @wca_db
 AND s.index_name != 'PRIMARY'
 GROUP BY t.table_schema, t.table_name, t.table_rows,  s.index_name
 ORDER BY t.table_schema, t.table_name, t.table_rows,  s.index_name;
 
 -- List tables without any indices
-SELECT t.table_schema, t.table_name, t.table_rows
+SELECT *, t.table_schema, t.table_name, t.table_rows
 FROM information_schema.tables t
 LEFT JOIN information_schema.statistics s ON s.table_schema = t.table_schema AND s.table_name = t.table_name
-WHERE t.table_schema = @wca_db
+WHERE t.engine = 'InnoDB'
+AND t.table_schema = @wca_db
 AND s.index_name IS NULL
 ORDER BY t.table_schema, t.table_name, t.table_rows;
 
--- List index sizes - PRIMARY is a clustered index so it includes the actual table records
-SELECT database_name, table_name, database_name, index_name,
-	ROUND(stat_value *
-		CASE
-            WHEN database_name = 'wcastats' THEN 8192
-			ELSE @@innodb_page_size
-		END / 1024 / 1024, 2) size_in_mb
-FROM mysql.innodb_index_stats
-WHERE database_name = @wca_db
+-- List index sizes - PRIMARY is a clustered index so it includes the actual records for the base table
+SELECT database_name, i.table_name, index_name, row_format,
+	ROUND(stat_value * CASE WHEN row_format = 'Compressed' THEN 8192 ELSE @@innodb_page_size END / 1024 / 1024, 2) size_in_mb
+FROM information_schema.tables t
+JOIN mysql.innodb_index_stats i ON i.database_name = t.table_schema AND i.table_name = t.table_name
+WHERE t.engine = 'InnoDB'
+AND t.table_schema = @wca_db
+AND t.table_name = 'Results'
 AND stat_name = 'size'
-ORDER BY database_name, table_name;
+ORDER BY table_name, index_name, database_name;
--- sql/check_tables.sql
+++ sql/check_tables.sql
@@ -4,21 +4,67 @@
     Author:   Michael George / 2015GEOR02
    
     Purpose:  Check table sizes
+
+    Notes:    The InnoDB engine stores data records in the clustered index (i.e. PRIMARY index)
 */
 
-SET @wca_db = 'wca';
+SET @wca_db = 'wca_alt';
 
--- Total table sizes in WCA database
-SELECT table_schema, ROUND(sum(data_length / 1024 / 1024), 2) AS data_length_mb
+/*
+    Overall database sizes
+*/
+
+-- Table sizes only
+SELECT table_schema, ROUND(sum(data_length / 1024 / 1024), 2) AS size_in_mb
 FROM information_schema.tables
-WHERE table_schema = @wca_db
+WHERE engine = 'InnoDB'
+AND table_schema LIKE 'wca%'
 GROUP BY table_schema
-ORDER BY table_schema;
+ORDER BY size_in_mb DESC;
 
--- Individual tables sizes should match the size of the clustered index (e.g. PRIMARY)
-SELECT table_schema, table_name, ROUND(data_length / 1024 / 1024, 2) AS data_length_mb
-FROM information_schema.tables
-WHERE table_schema = @wca_db
-ORDER BY table_schema, table_name;
+-- Table sizes + index sizes
+SELECT database_name,
+	ROUND(SUM(stat_value * CASE WHEN row_format = 'Compressed' THEN 8192 ELSE @@innodb_page_size END / 1024 / 1024), 2) size_in_mb
+FROM information_schema.tables t
+JOIN mysql.innodb_index_stats i ON i.database_name = t.table_schema AND i.table_name = t.table_name
+WHERE t.engine = 'InnoDB'
+AND t.table_schema LIKE 'wca%'
+AND stat_name = 'size'
+GROUP BY database_name
+ORDER BY size_in_mb DESC;
 
+/*
+    Individual table sizes
+*/
 
+-- Table sizes only
+WITH stats AS
+(
+	SELECT table_schema, table_name, row_format, ROUND(data_length / 1024 / 1024, 2) AS size_in_mb
+	FROM information_schema.tables
+	WHERE table_schema IN ('wca', 'wca_alt')
+	AND engine = 'InnoDB'
+)
+SELECT *, ROUND(100.0 * s1.size_in_mb / s2.size_in_mb, 2) AS pctSize
+FROM stats s1
+LEFT JOIN stats s2 ON s2.table_name = s1.table_name AND s2.table_schema = 'wca'
+WHERE s1.table_schema = 'wca_alt'
+ORDER BY s1.size_in_mb DESC;
+
+-- Table sizes + index sizes
+WITH stats AS
+(
+	SELECT table_schema, i.table_name, row_format,
+		ROUND(SUM(stat_value * CASE WHEN row_format = 'Compressed' THEN 8192 ELSE @@innodb_page_size END / 1024 / 1024), 2) size_in_mb
+	FROM information_schema.tables t
+	JOIN mysql.innodb_index_stats i ON database_name = table_schema AND i.table_name = t.table_name
+	WHERE table_schema IN ('wca', 'wca_alt')
+	AND engine = 'InnoDB'
+	AND stat_name = 'size'
+	GROUP BY database_name, table_name
+)
+SELECT *, ROUND(100.0 * s1.size_in_mb / s2.size_in_mb, 2) AS pctSize
+FROM stats s1
+LEFT JOIN stats s2 ON s2.table_name = s1.table_name AND s2.table_schema = 'wca'
+WHERE s1.table_schema = 'wca_alt'
+ORDER BY s1.size_in_mb DESC;
