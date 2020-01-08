6cc28fc24dc540a0c1ecde89b32ac76894515897
Logiqx <software@mikeg.me.uk>
2020-01-08 19:45:19 +0000

    Remove redundant message

--- bin/env.sh
+++ bin/env.sh
@@ -7,7 +7,6 @@ run_py_script()
   IMAGE_NAME=${PROJ_NAME}
   IMAGE_TAG=${IMAGE_TAG:-latest}
 
-  echo "Running $1..."
   docker run -it --rm \
          --mount type=bind,src=$(realpath $PROJ_DIR/docker/mysql/.my.cnf),dst=/home/jovyan/.my.cnf \
          --network=wca_default -w $WORK_DIR/python $IMAGE_NAME:$IMAGE_TAG ./$1
