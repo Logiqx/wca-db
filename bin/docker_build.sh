26119bcac30636582fae65000845913d761747bd
Logiqx <software@mikeg.me.uk>
2020-01-08 16:35:20 +0000

    Download database after Docker build, prior to tagging the image

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,11 +1,18 @@
+set -ex
+
+# Project Env
 . $(dirname $0)/env.sh
-
-set -e
-
 cd $PROJ_DIR
 
-DOCKER_BUILDKIT=1 docker build . -t $PROJ_NAME:$(git rev-parse --short=12 HEAD)
+# Determine Tag
+IMAGE_NAME=$PROJ_NAME
+IMAGE_TAG=$(git rev-parse --short=12 HEAD)
 
+# Docker Build
+DOCKER_BUILDKIT=1 docker build . -t $IMAGE_NAME:$IMAGE_TAG
+
+# Download Database
 time run_py_script Download_Results.py
 
-docker tag $PROJ_NAME:$(git rev-parse --short=12 HEAD) $PROJ_NAME:latest
+# Docker Tag
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
--- bin/env.sh
+++ bin/env.sh
@@ -4,8 +4,11 @@ export WORK_DIR=/home/jovyan/work/$PROJ_NAME
 
 run_py_script()
 {
+  IMAGE_NAME=${PROJ_NAME}
+  IMAGE_TAG=${IMAGE_TAG:-latest}
+
   echo "Running $1..."
   docker run -it --rm \
          --mount type=bind,src=$(realpath $PROJ_DIR/docker/mysql/.my.cnf),dst=/home/jovyan/.my.cnf \
-         --network=wca_default -w $WORK_DIR/python $PROJ_NAME ./$1
+         --network=wca_default -w $WORK_DIR/python $IMAGE_NAME:$IMAGE_TAG ./$1
 }
