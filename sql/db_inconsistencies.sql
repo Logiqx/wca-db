f2247bc9e8e3341c6c37ea3e0c93df830c7a1940
Logiqx <software@mikeg.me.uk>
2026-01-07 13:17:09 +0000

    Improve SQL checks

--- sql/db_inconsistencies.sql
+++ sql/db_inconsistencies.sql
@@ -11,36 +11,36 @@
 --     excludes persons, ranks_average, ranks_single, ar_internal_metadata, eligible_country_iso2s_for_championship, schema_migrations
 SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS AS c1
-WHERE table_schema LIKE 'wca%'
-AND column_name = 'id'
-ORDER BY TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME;
+WHERE TABLE_SCHEMA IN ('wca', 'wca_dev')
+AND COLUMN_NAME = 'id'
+ORDER BY TABLE_SCHEMA, DATA_TYPE, TABLE_NAME, COLUMN_NAME;
 
 -- Check which tables have a `person_id` column - PROPOSE rename of `person_id` to `wca_id`
 --   3 tables in results export / 6 tables in developer export
 --     ranks_average, ranks_single, results + concise_average_results, concise_single_results, inbox_results
 SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS AS c1
-WHERE table_schema LIKE 'wca%'
-AND column_name = 'person_id'
-ORDER BY TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME;
+WHERE TABLE_SCHEMA IN ('wca', 'wca_dev')
+AND COLUMN_NAME = 'person_id'
+ORDER BY TABLE_SCHEMA, DATA_TYPE, TABLE_NAME, COLUMN_NAME;
 
 -- Check which tables have a `wca_id` column - PROPOSE rename of `person_id` to `wca_id`
 --   1 table in results export / 4 tables in developer export
 --     persons + inbox_persons, tickets_edit_person, users
 SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS AS c1
-WHERE table_schema LIKE 'wca%'
-AND column_name = 'wca_id'
-ORDER BY TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME;
+WHERE TABLE_SCHEMA IN ('wca', 'wca_dev')
+AND COLUMN_NAME = 'wca_id'
+ORDER BY TABLE_SCHEMA, DATA_TYPE, TABLE_NAME, COLUMN_NAME;
 
 -- Check which tables have `day`, `month`, `year` columns - PROPOSE concise results, competitions and persons use proper date fields
 --   1 table in results export / 2 tables in developer export
 --     competitions / concise_average_results, concise_single_results
 SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS AS c1
-WHERE table_schema LIKE 'wca%'
-AND column_name = 'month'
-ORDER BY TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME;
+WHERE TABLE_SCHEMA IN ('wca', 'wca_dev')
+AND (COLUMN_NAME LIKE '%day' OR COLUMN_NAME LIKE '%month' OR COLUMN_NAME LIKE '%year')
+ORDER BY TABLE_SCHEMA, TABLE_NAME, DATA_TYPE, LENGTH(COLUMN_NAME), COLUMN_NAME;
 
 -- Compare tables in results export against developer export - PROPOSE competitions and persons use proper date fields
 --   2 tables in results export have columns that are not in the developer export
@@ -56,4 +56,4 @@ AND NOT EXISTS
     AND c2.table_name = c1.table_name
     AND c2.column_name = c1.column_name
 )
-ORDER BY TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME;
+ORDER BY TABLE_SCHEMA, TABLE_NAME, DATA_TYPE, LENGTH(COLUMN_NAME), COLUMN_NAME;
